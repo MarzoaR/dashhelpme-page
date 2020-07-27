f2ceac24772ba060b11adf9dd80acbcc2019593d
Roberto Marzoa <robertomarzoa@gmail.com>
2020-07-27 19:39:11 -0400

    Se creo el menu y sus respectivos estilos en el archivo menu.css, adicionalmente se agregaron funcionalidades en app.js, el consumo del api para obtener los valores de dash en dolares y bolivares, y la modificacion del menu al hacer scroll

--- js/app.js
+++ js/app.js
@@ -1,17 +1,51 @@
-// Scroll menu
+// Moidificación de la barra de nevagacion según el scroll
 
-// window.addEventListener('scroll', () => {
-//   const nav = document.querySelector('nav')
-//   const img = document.querySelector('.nav__logo img')
-//   nav.classList.toggle('sticky', window.scrollY > 100)
-//   if(window.scrollY > 100) {
-//     img.setAttribute('src', './img/logo/dashHelpColor.svg')
-//   } else {
-//     img.setAttribute('src', './img/logo/dashHelpWhite.svg')
-//   }
-// })
+window.addEventListener('scroll', () => {
+  const nav = document.querySelector('.menu');
+  const img = document.querySelector('.menu_logo img');
+  const dDown = document.querySelectorAll('.menu_list__dropdown');
 
+  nav.classList.toggle('sticky', window.scrollY > 100)
 
+  dDown.forEach(element => {
+    element.classList.toggle('dropdown', window.scrollY > 100);
+  });
+
+  if(window.scrollY > 100) {
+    img.setAttribute('src', './img/logo/dashHelpColor.svg');
+    nav.setAttribute('height', '80px');
+    img.setAttribute('width', '80%');
+  } else {
+    img.setAttribute('src', './img/logo/dashHelpWhite.svg');
+    nav.setAttribute('height', '100px');
+    img.setAttribute('width', '100%');
+  }
+});
+
+// Obtoner valor dash en dolares y bolivares
+
+let currentcy_dollar = document.querySelector('#currentcy_dollar');
+let currentcy_ves = document.querySelector('#currentcy_ves');
+
+  function getCurrentcy() {
+    const API = 'https://rates2.dashretail.org/rates?symbol=dashves&nested=1';
+
+    fetch(API)
+      .then(response => response.json())
+      .then(myJson => {
+        let USD, VES;
+        let usd = myJson[0].nested[0].price;
+        let ves = myJson[0].price;
+        USD = new Intl.NumberFormat("de-DE", {style: "currency", currency: "USD"}).format(usd);
+        VES = new Intl.NumberFormat("de-DE", {style: "currency", currency: "VES"}).format(ves);
+
+        currentcy_dollar.innerHTML = `${USD}`;
+        currentcy_ves.innerHTML = `${VES}`;
+        // console.log(usd);
+      });
+  }
+  getCurrentcy();
+  setInterval (getCurrentcy, 10000);
 
 
 
