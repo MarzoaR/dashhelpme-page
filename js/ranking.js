4f1795870fb1ea55e1f277ce81dbdca5237115e3
Roberto Marzoa <robertomarzoa@gmail.com>
2020-09-09 18:21:04 -0400

    Se actualizó el index.html con los valores reales del ranking de dash consumido por el api en https://dash-utils-api.herokuapp.com/API/rates, se creó el archivo ranking.js para controlar esta accion

---
+++ js/ranking.js
@@ -0,0 +1,24 @@
+const api = 'https://dash-utils-api.herokuapp.com/API/rates'
+
+let rank = document.querySelector('#listRanking')
+let capitalization = document.querySelector('#listCapitalizacion')
+let volume = document.querySelector('#listVolumen')
+
+function ranking(API) {
+
+
+  fetch(API)
+    .then(response => response.json())
+    .then(myrank => {
+
+      rank.innerHTML = `#${myrank.ranking}`
+      capitalization.innerHTML = `+$${myrank.cap}MM`
+      volume.innerHTML = `+$${myrank.volume}MM`
+
+    }).catch(err => {
+      console.log(err)
+    })
+}
+
+ranking(api)
+
